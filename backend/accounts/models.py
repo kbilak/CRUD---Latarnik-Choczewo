53247c26007b70d11ec81840709cfcae655c73cd
Krzysztof Białk <krzys.bialk1@gmail.com>
2023-12-18 16:52:45 +0100

    add user model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import uuid
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    USER_TYPES = (
+        ('admin', 'Admin'),
+        ('normal', 'Normal'),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
+    email = models.CharField(max_length=255, unique=True)
+    password_hash = models.CharField(max_length=255)
+    date_joined = models.DateTimeField(default=timezone.now, verbose_name='Data dołączenia')
+    user_type = models.CharField(choices=USER_TYPES, max_length=255, default='individual', verbose_name='Typ użytkownika')
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        ordering = ('-date_joined',)
+        verbose_name = 'Użytkownik'
+        verbose_name_plural = 'Użytkownicy'
